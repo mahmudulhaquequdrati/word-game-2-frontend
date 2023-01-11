ac02f20e5ddf424686b839ae6032a53ef80fc049
Riyadh <riyadh.ju@gmail.com>
2023-01-11 23:11:21 +0600

    added front page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Days from "../components/days/Days";
+import Home from "../components/home/Home";
 import Login from "../components/login/Login";
 import Register from "../components/register/Register";
 
 export const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home></Home>,
+  },
   {
     path: "/login",
     element: <Login></Login>,
