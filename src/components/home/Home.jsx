cad12605028fc9f6ddafd025fc61268144b90940
Riyadh <riyadh.ju@gmail.com>
2023-01-14 23:43:55 +0600

    added styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,14 +4,32 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>ok</h1>
+    <div className="top-conatiner">
       <div className="context">
-        <h3>Pass Gas</h3>
-        <p>Press Go to start the clock</p>
+        <div>
+          <div>
+            <h3>Pass Gas</h3>
+            <p>Press Go to start the clock</p>
+          </div>
+        </div>
         <Link to="/word-match">
-          <button>Go</button>
+          <button>Start</button>
         </Link>
+      </div>
+
+      <div class="area">
+        <ul class="circles">
+          <li>A</li>
+          <li>L</li>
+          <li>Z</li>
+          <li>E</li>
+          <li>P</li>
+          <li>F</li>
+          <li>O</li>
+          <li>W</li>
+          <li></li>
+          <li>X</li>
+        </ul>
       </div>
     </div>
   );
